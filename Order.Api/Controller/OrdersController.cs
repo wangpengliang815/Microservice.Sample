ab383bd705e805cbda103366876e3accb06246af
WangPengLiang <15101587969@163.com>
2021-09-16 13:52:02 +0800

    添加项目文件。

---
+++ Order.Api/Controller/OrdersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System;
+
+namespace Order.Api.Controller
+{
+    [Route("[Controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            string result = $"订单服务：{DateTime.Now:yyyy-MM-dd HH:mm:ss},-{Request.HttpContext.Connection.LocalIpAddress}:{Request.HttpContext.Connection.LocalPort}";
+            return Ok(result);
+        }
+    }
+}
---
+++ Product.Api/Controller/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System;
+
+namespace Order.Api.Controller
+{
+    [Route("[Controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            string result = $"产品服务：{DateTime.Now:yyyy-MM-dd HH:mm:ss},-{Request.HttpContext.Connection.LocalIpAddress}:{Request.HttpContext.Connection.LocalPort}";
+            return Ok(result);
+        }
+    }
+}
